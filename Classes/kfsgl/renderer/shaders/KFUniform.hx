91947bb3b45211a68d45280c0b0b3e4008dd3f17
Stuart Caunt <stuart.caunt@gmail.com>
2015-02-04 23:50:36 +0100

    Improved uniform

--- Classes/kfsgl/renderer/shaders/KFUniform.hx
+++ Classes/kfsgl/renderer/shaders/KFUniform.hx
@@ -1,5 +1,8 @@
 package kfsgl.renderer.shaders;
 
+import haxe.Json;
+import openfl.geom.Matrix3D;
+
 import kfsgl.renderer.shaders.KFUniformLib;
 import kfsgl.utils.KF;
 import kfsgl.errors.KFException;
@@ -10,12 +13,17 @@ class KFUniform  {
 	private var _uniformInfo:KFUniformInfo;
 	private var _location:Int;
 	private var _size:Int;
-	private var _floatValue:Array<Float> = new Array<Float>();
 
-	private var _defaultFloatValue:Array<Float> = new Array<Float>();
+	private var _floatValue:Float = 0.0;
+	private var _floatArrayValue:Array<Float> = new Array<Float>();
+	private var _matrixValue:Matrix3D = new Matrix3D();
 
-	private var _hasBeenSet:Bool;
-	private var _isDirty:Bool;
+	private var _defaultFloatValue:Float;
+	private var _defaultFloatArrayValue:Array<Float> = new Array<Float>();
+	private var _defaultMatrixValue:Matrix3D = new Matrix3D();
+
+	private var _hasBeenSet:Bool = false;
+	private var _isDirty:Bool = true;
 
 	public function new(name:String, uniformInfo:KFUniformInfo, location:Int) {
 		_name = name;
@@ -24,9 +32,7 @@ class KFUniform  {
 
 		handleDefaultValue();
 
-
-		KF.Log("uniform " + _name + " (" + _defaultFloatValue + ")" + " : " + _uniformInfo.name + " = " + _location);
-
+		KF.Log(toString());
 	}
 
 
@@ -41,11 +47,13 @@ class KFUniform  {
 
 	public function use() {
 		// If hasn't been set then use the default value
-		if (!_hasBeenSet && _defaultFloatValue.length > 0) {
+		if (!_hasBeenSet) {
 			if (_size == 1) {
-				setValue(_defaultFloatValue[0]);
+				setValue(_defaultFloatValue);
+			} else if (_size < 16) {
+				setArrayValue(_defaultFloatArrayValue);
 			} else {
-				setValues(_defaultFloatValue);
+				setMatrixValue(_defaultMatrixValue);
 			}
 		}
 
@@ -61,31 +69,42 @@ class KFUniform  {
 		} else {
 			_hasBeenSet = true;
 
-			if (_floatValue[0] != value) {
-				_floatValue[0] = value;
+			if (_floatValue != value) {
+				_floatValue = value;
 				_isDirty = true;
 			}
 		}
 	}
 
-	public function setValues(values:Array<Float>) {
-		if (_size == 1) {
-			throw new KFException("IncoherentUniformValue", "An array value is being set for the uniform float " + _uniformInfo.name);
+	public function setArrayValue(value:Array<Float>) {
+		if (_size == 1 || _size == 16) {
+			throw new KFException("IncoherentUniformValue", "A float or matrix value is being set for the array uniform " + _uniformInfo.name);
 		
-		} else if (_size != values.length) {
-			throw new KFException("IncoherentUniformValue", "An array of size " + values.length + " is being set for the uniform array " + _uniformInfo.name + " with size " + _size);
+		} else if (_size != value.length) {
+			throw new KFException("IncoherentUniformValue", "An array of size " + value.length + " is being set for the uniform array " + _uniformInfo.name + " with size " + _size);
 		
 		} else {
 			_hasBeenSet = true;
 
 			// Comparison of both arrays
-			if (values.toString() != _floatValue.toString()) {
-
+			if (value.toString() != _floatArrayValue.toString()) {
 				// Copy array values
-				_floatValue = values.copy();
+				_floatArrayValue = value.copy();
 				_isDirty = true;
 			}
 
+		}
+	}
+
+	public function setMatrixValue(value:Matrix3D) {
+		_hasBeenSet = true;
+
+		// Comparison of both matrices
+		if (value.rawData.toString() != _matrixValue.rawData.toString()) {
+
+			// Copy array values
+			_matrixValue.copyFrom(value);
+			_isDirty = true;
 		}
 	}
 
@@ -95,43 +114,80 @@ class KFUniform  {
 			var type = _uniformInfo.type;
 
 			if (type == "float") {
+				_size = 1;
 				var floatValue:Float = Std.parseFloat(defaultValue);
 				if (floatValue == Math.NaN) {
 					throw new KFException("UnableToParseUniformValue", "Could not parse default value " + defaultValue + " for uniform " + _uniformInfo.name);
 
 				} else {
-					_floatValue.push(floatValue);
+					_defaultFloatValue = floatValue;
+					setValue(_defaultFloatValue);
 				}
-				_size = 1;
-				
+
 			} else if (type == "vec2") {
-				_defaultFloatValue = haxe.Json.parse(defaultValue);
+				_defaultFloatArrayValue = haxe.Json.parse(defaultValue);
 				_size = 2;
-				
+				setArrayValue(_defaultFloatArrayValue);
+
 			} else if (type == "vec3") {
-				_defaultFloatValue = haxe.Json.parse(defaultValue);
+				_defaultFloatArrayValue = haxe.Json.parse(defaultValue);
 				_size = 3;
+				setArrayValue(_defaultFloatArrayValue);
 
 			} else if (type == "ve4") {
-				_defaultFloatValue = haxe.Json.parse(defaultValue);
+				setArrayValue(_defaultFloatArrayValue);
+				_defaultFloatArrayValue = haxe.Json.parse(defaultValue);
 				_size = 4;
-				
+				setArrayValue(_defaultFloatArrayValue);
+
 			} else if (type == "mat3") {
 				if (defaultValue == "identity") {
 					defaultValue = "[1, 0, 0, 0, 1, 0, 0, 0, 1]";
 				}
-				_defaultFloatValue = haxe.Json.parse(defaultValue);
+				_defaultFloatArrayValue = haxe.Json.parse(defaultValue);
 				_size = 9;
-				
+				setArrayValue(_defaultFloatArrayValue);
+
 			} else if (type == "mat4") {
 				if (defaultValue == "identity") {
 					defaultValue = "[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]";
 				}
-				_defaultFloatValue = haxe.Json.parse(defaultValue);
+				var floatArray:Array<Float> = haxe.Json.parse(defaultValue);
+				_defaultMatrixValue.copyRawDataFrom(floatArray);
 				_size = 16;
+				setMatrixValue(_defaultMatrixValue);
 
 			}
 		}
+	}
+
+	public function toString() {
+		var type = _uniformInfo.type;
+		var text:String = "unifom " + _name + " (";
+
+		if (type == "float") {
+			text += _floatValue;
+
+		} else if (type == "vec2") {
+			text += _floatArrayValue.toString();
+
+		} else if (type == "vec3") {
+			text += _floatArrayValue.toString();
+
+		} else if (type == "ve4") {
+			text += _floatArrayValue.toString();
+
+		} else if (type == "mat3") {
+			text += _floatArrayValue.toString();
+
+		} else if (type == "mat4") {
+			text += _matrixValue.rawData.toString();
+
+		}
+
+		text += ")" + " : " + _uniformInfo.name + " = " + _location;
+
+		return text;
 	}
 
 }
