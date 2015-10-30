89c65e09bb4548bcfb1f3cb9186c9af4bf262e15
Stuart Caunt <stuart.caunt@gmail.com>
2015-10-30 22:08:13 +0100

    Save screen capture in html5.

--- xt3d/utils/image/ScreenCapture.hx
+++ xt3d/utils/image/ScreenCapture.hx
@@ -1,8 +1,8 @@
 package xt3d.utils.image;
 
-import sys.FileSystem;
 import lime.system.System;
 #if !js
+import sys.FileSystem;
 import sys.io.File;
 import sys.io.FileOutput;
 #end
@@ -21,6 +21,11 @@ class ScreenCapture {
 	private var _renderTexture:RenderTexture = null;
 	private var _pixels:UInt8Array = null;
 	private var _flippedPixels:UInt8Array = null;
+#if js
+	private var _canvas:Dynamic;
+	private var _context:Dynamic;
+	private var _contextPixels:Dynamic;
+#end
 
 	public static function create():ScreenCapture {
 		var object = new ScreenCapture();
@@ -61,21 +66,57 @@ class ScreenCapture {
 	}
 
 	public function save(path:String = null, format:String = "png", quality:Int = 90):Void {
-#if js
-		XT.Warn("ScreenCapture.save is not available for the HTML5 target");
-#else
 		var width = this._renderTexture.contentSize.width;
 		var height = this._renderTexture.contentSize.height;
 		var nPixels = width * height * 4;
 		if (this._pixels == null || this._pixels.length != nPixels) {
 			this._pixels = new UInt8Array(nPixels);
+
+#if js
+			// Create javascript canvas and 2d context with image data
+			this._canvas = untyped __js__("document.createElement('canvas');");
+			untyped __js__("this._canvas.width = {0};", width);
+			untyped __js__("this._canvas.height = {0};", height);
+			this._context = untyped __js__("this._canvas.getContext('2d');");
+			this._contextPixels = untyped __js__("this._context.createImageData({0}, {1});", width, height);
+
+#else
+			// Additional data to invert texture
 			this._flippedPixels = new UInt8Array(nPixels);
+#end
 		}
 
 		// Read pixels from texture
 		GL.readPixels(0, 0, width, height, GL.RGBA, GL.UNSIGNED_BYTE, this._pixels);
 
-		// Flip vertically
+#if js
+		// Flip pixels vertically and save in context image data
+		for (i in 0 ... height) {
+			var src = i * width * 4;
+			var dest = (height - 1 - i) * width * 4;
+			this._contextPixels.data.set(this._pixels.subarray(src, src + (width * 4)), dest);
+		}
+
+		// Put data into context
+		untyped __js__("this._context.putImageData(this._contextPixels, 0, 0);");
+
+		// Create a data uri from the image data
+		var uri = untyped __js__("this._canvas.toDataURL();");
+
+		// Open a new tab with the image
+		//untyped __js__("window.open({0}, 'ScreenCapture');", uri);
+
+		// Filename
+		var filename = DateTools.format(Date.now(), "xTalk3d Screen Capture %Y-%m-%d at %H.%M.%S.png");
+
+		// Force a download of the image
+		var link = untyped __js__("document.createElement('a');");
+		untyped __js__("link.download = {0};", filename);
+		untyped __js__("link.href = {0};", uri);
+		untyped __js__("link.click();");
+#else
+
+		// Flip pixels vertically
 		for (i in 0 ... height) {
 			var src = i * width * 4;
 			var dest = (height - 1 - i) * width * 4;
