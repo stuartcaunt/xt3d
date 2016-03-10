cd3bbe3ac9e2e9ca1322332f1d7b9104f248e2ca
Stuart Caunt <stuart.caunt@gmail.com>
2016-03-10 20:25:04 +0100

    Remove inefficient depth of field filter (using separate blur render target) and test.

--- Assets/shaders/depthOfFieldBasic_vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 v_uv;
-
-void main() {
-	// Set uv
-	v_uv = a_uv * u_uvScaleOffset.xy + u_uvScaleOffset.zw;
-
-	// Projected position
-	gl_Position = u_modelViewProjectionMatrix * a_position;
-}
